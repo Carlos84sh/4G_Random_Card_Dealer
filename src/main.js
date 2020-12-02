ab20002b8622e3dba0e8f91e58040e28cc16243f
Carlos84sh <Cs.huelves@gmail.com>
2020-12-02 07:51:43 +0000

    primera correcion

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
-require('./style.css')
+require('./style.css');
 
 window.onload = function() {
     
+    let CardType = () => {
+        let cardTypeList = ["clubs", "diamantes", "hearts", "spades"];
+        let randomType = Math.floor(Math.random() * cardTypeList.length);
+        
+        return cardTypeList[randomType];
+    }
 
+    let CardNum = () => {
+        let cardNumList = ["A","1","2","3","4","5","6","7","8","9","10","J","Q","K"];
+        let randomNum = Math.floor(Math.random() * cardNumList.length);
+    
+        return cardNumList[randomNum];
+    }
 
+    let card = document.querySelector(".card");
+    let content = document.createElement("span");
+    card.appendChild(content);
+
+    content.innerHTML = CardNum();
     
-    
+    card.classList.add(CardType())
 }
